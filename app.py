30b589921ae820c5fad222630439aa73b41d8993
Karen Javadyan <kajarenc@gmail.com>
2023-09-08 19:29:53 +0400

    change app file

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
+st.title("DOWNLOAD BUTTON _SELF AND DOWNLOAD ATTRIBUTE DEMO")
+
 x = st.slider("AAA", 0, 100, 27)
-st.download_button("AAAAA", "BBBBBBBBBB" * x)
+st.download_button("AAAAA", "BBBBBBBBBBAAABB" * x)
